1b6eb299db439a89bab72f0b63e458c244f755be
Tyapor718 <katepsupport@mail.com>
2023-04-17 01:42:56 +0400

    Упорядочивание строк массива по убыванию

---
+++ ex01/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+int[,] CreateArray(int masRows, int masColumns)
+{
+    int[,] array = new int[masRows, masColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-10, 10);
+        }
+    }
+    return array;
+}
+void Print2DArrayMas(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+int[,] DescendingMassiv(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int rowMas = ReadInt("Количество строк массива  >");
+int columnMas = ReadInt("Количесвто столбцов массива >");
+int[,] massive = CreateArray(rowMas, columnMas);
+Print2DArrayMas(massive);
+Print2DArrayMas(DescendingMassiv(massive));
+
